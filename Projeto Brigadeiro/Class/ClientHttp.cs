d903e986ccb8f0a67872c66f0748228089d1ff71
Roberto Macedo <robertoneto5@hotmail.com>
2024-05-16 21:08:39 -0300

    Criado classe para consulta da API para usar a mesma instancia do objeto ClientHttp

---
+++ Projeto Brigadeiro/Class/ClientHttp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Projeto_Brigadeiro.Class
+{
+    public class ClientHttp
+    {
+        public static HttpClient Client { get; private set; }
+
+        public ClientHttp()
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://192.168.68.12:8080/api/v1/");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
